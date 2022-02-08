8b46364570a470dd4137266c7a6cc0673e7c6e86
captainAyan <ayanchakraborty105@gmail.com>
2022-02-08 14:00:20 +0530

    feat: :sparkles: added data types and data structures (very basic)

---
+++ fundamental/dataTypeAndDataStructure.js
@@ -0,0 +1,25 @@
+/**
+ * Data Types and Data Structures
+ *
+ * MDN Web Docs
+ * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Data_structures
+ */
+
+// Dynamic typing
+let foo = 42;    // foo is now a number
+foo     = 'bar'; // foo is now a string
+foo     = true;  // foo is now a boolean
+
+/**
+ * TYPES
+ *
+ * - Primitive values
+ * --- Boolean
+ * --- Null
+ * --- Undefined
+ * --- Number
+ * --- String
+ * --- Symbol
+ *
+ * - Objects (Non-primitive)
+ */
