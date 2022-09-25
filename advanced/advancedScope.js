ae1127f5b8b6e4b2120d10f6a1c3b9c45995b98c
captainAyan <ayanchakraborty105@gmail.com>
2022-09-26 02:08:31 +0530

    feat: :sparkles: added more details

--- advanced/advancedScope.js
+++ advanced/advancedScope.js
@@ -32,8 +32,27 @@ function myFunction() {
 console.log(c); // Error: c is not defined
 
 // Global Scope
-var x = 2;       // Global scope
-let y = 2;       // Global scope
-const z = 2;     // Global scope
+var x = 2; // Global scope
+let y = 2; // Global scope
+const z = 2; // Global scope
 // Global variables can be accessed from anywhere.
 
+// RE-Declaration
+var a = 1;
+var a = 2; // this won't invoke an error
+
+let b = 1;
+let b = 2; // this will invoke an error
+
+const c = 1;
+const c = 1; // this will invoke an error
+
+// RE-Assignment
+var p = 1;
+p = 2; // this won't invoke an error
+
+let q = 1;
+q = 2; // this won't invoke an error
+
+const r = 1;
+r = 2; // this will invoke an error
